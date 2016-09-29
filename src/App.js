0b2126959261eca51859bc944f10dbd3976df195
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-09-29 14:10:19 -0700

    group colors by trip, start on radial layout

--- src/App.js
+++ src/App.js
@@ -6,47 +6,81 @@ import chroma from 'chroma-js';
 import Canvas from './Canvas';
 import photos from './data/colors.json';
 
-var width = 2000;
-var height = 2000;
+var width = 1500;
+var height = 1800;
 var backgroundColor = chroma('#273547').darken().hex();
-var xScale = d3.scaleLinear().domain([-180, 180]).range([0, width]);
-var timeScale = d3.scaleTime()
-  .domain([new Date('1/1/2012'), new Date('12/31/2016')])
-  .range([0, height]);
+var fontColor = chroma('#273547').brighten(4).hex();
+
+// var xScale = d3.scaleLinear().domain([-180, 180]).range([0, width]);
+var timeScale = d3.scaleTime().range([0, 360]);
 var App = React.createClass({
   getInitialState() {
     return {
       width,
       height,
-      colors: [],
+      fontColor,
+      trips: [],
     };
   },
 
   componentWillMount() {
-    var colors = _.chain(photos)
-      .map((photo) => {
-        return _.map(photo.colors, color => {
-          if (chroma.contrast(backgroundColor, color) < 4.5) {
-            // if contrast is low, up the saturation
-            color = chroma(color).brighten().saturate(2).hex();
-          } else {
-            color = chroma(color).saturate(2).hex();
-          }
-          var date = photo.date && new Date(photo.date);
-          if ((date ? timeScale(date) : 0) < 0) debugger
-          return {
-            id: photo.id,
-            tripId: photo.tripId,
-            date,
-            geo: photo.geo,
-            focusX: photo.geo ? xScale(photo.geo[1]) : 0,
-            focusY: date ? timeScale(date) : 0,
-            color: color,
-          };
-        });
-      }).flatten().value();
+    var index = 0;
+    var perRow = 3;
+    var tripSize = 400;
 
-    this.setState({colors});
+    var trips = _.chain(photos)
+      .filter(photo => {
+        photo.date = photo.date && new Date(photo.date);
+        return photo.date;
+      }).groupBy(photo => photo.tripId)
+      .map((photos, tripId) => {
+        // first calculate trip position
+        var x = (index % perRow + .5) * tripSize;
+        var y = (Math.floor(index / perRow) + .5) * tripSize;
+        index += 1;
+        // then get start and end dates of the trip
+        var startDate = _.minBy(photos, photo => photo.date).date;
+        var endDate = _.maxBy(photos, photo => photo.date).date;
+        startDate = d3.timeDay.floor(startDate);
+        endDate = d3.timeDay.ceil(endDate);
+        timeScale.domain([startDate, endDate]);
+
+        var colors = _.chain(photos)
+          .map((photo) => {
+            var hour = d3.timeHour.floor(photo.date);
+            var angle = timeScale(hour);
+            var focusX = -Math.cos(angle) * (tripSize / 2);
+            var focusY = Math.sin(angle) * (tripSize / 2);
+
+            return _.map(photo.colors, color => {
+              if (chroma.contrast(backgroundColor, color) < 4.5) {
+                // if contrast is low, up the saturation
+                color = chroma(color).brighten().saturate(2).hex();
+              } else {
+                color = chroma(color).saturate(2).hex();
+              }
+
+              return {
+                id: photo.id,
+                tripId,
+                date: photo.date,
+                focusX,
+                focusY,
+                geo: photo.geo,
+                color: color,
+              };
+            });
+          }).flatten().value();
+
+        return {
+          x,
+          y,
+          colors,
+          id: tripId,
+        }
+      }).value();
+
+    this.setState({trips});
   },
 
   render() {
