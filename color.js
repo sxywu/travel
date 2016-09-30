f7efdf67ec58d43139a1b66a05ebbc43201c66a5
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-09-29 20:41:28 -0700

    color.js: account for addition of filename

--- color.js
+++ color.js
@@ -7,10 +7,10 @@ var newMeta = [];
 function getColorsForImage(m, meta) {
   if (_.isEmpty(meta)) {
     // if there's no morew photos, save it!
-    fs.writeFile('metadata.json', JSON.stringify(newMeta), 'utf8');
+    fs.writeFile('colors.json', JSON.stringify(newMeta), 'utf8');
     return;
   }
-  getColors('src/img/' + m.id, function(err, colors){
+  getColors('processed/' + m.id, function(err, colors){
     // colors is an array of colors
     colors = _.map(colors, function(color) {
       return color.hex();
@@ -24,21 +24,22 @@ function getColorsForImage(m, meta) {
   });
 }
 
-fs.readFile('src/data/metadata.json', 'utf8', function(err, metadata) {
+fs.readFile('metadata.json', 'utf8', function(err, metadata) {
   metadata = JSON.parse(metadata);
   var meta = _.chain(metadata)
     .map(function(photos, tripId) {
       return _.map(photos, function(photo) {
-        var date = photo[1] && photo[1].split(' ');
+        var date = photo[2] && photo[2].split(' ');
         var time = date && date[1];
         date = date && date[0].replace(/:/g, '/');
         date = date && date + ' ' + time;
 
         return {
           id: photo[0],
+          filename: photo[1],
           tripId: tripId,
           date: date,
-          geo: photo[2],
+          geo: photo[3],
         }
       });
     }).flatten().value();
